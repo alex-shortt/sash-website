523cc7ecebbd8095595ba40bd936628770d6998b
Alex Shortt <alexander.shortt@gmail.com>
2019-09-13 17:58:42 -0700

    move containers to separate component

---
+++ src/components/Containers/index.js
@@ -0,0 +1,19 @@
+import styled from "styled-components/macro"
+
+export const Landing = styled.div`
+  width: 100%;
+  height: 100vh;
+  background: linear-gradient(180deg, #598ac2, #639cdf);
+`
+
+export const Gap = styled.div`
+  width: 100%;
+  height: 80vh;
+  background: linear-gradient(180deg, #639cdf, #48c5ee);
+`
+
+export const Content = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  background: linear-gradient(180deg, #48c5ee, #4dd2fa);
+`
--- src/scenes/View/index.js
+++ src/scenes/View/index.js
@@ -3,24 +3,7 @@ import styled from "styled-components/macro"
 
 import Helmet from "components/Helmet"
 import Intro from "scenes/Intro"
-
-const Landing = styled.div`
-  width: 100%;
-  height: 100vh;
-  background: linear-gradient(180deg, #598ac2, #639cdf);
-`
-
-const Gap = styled.div`
-  width: 100%;
-  height: 80vh;
-  background: linear-gradient(180deg, #639cdf, #48c5ee);
-`
-
-const Content = styled.div`
-  width: 100%;
-  min-height: 100vh;
-  background: linear-gradient(180deg, #48c5ee, #4dd2fa);
-`
+import { Landing, Gap, Content } from "components/Containers"
 
 export default function View(props) {
   return (
