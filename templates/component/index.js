ecc077b3017f0a7b82aca376dde32d2b8ef6491b
Alex Shortt <alexander.shortt@gmail.com>
2019-09-25 11:20:47 -0700

    fix templates

--- templates/component/index.js
+++ templates/component/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import tw from "tailwind.macro"
 import styled from "styled-components/macro"
 
 export default function __TEMPLATE_NAME__(props) {
--- templates/scene/index.js
+++ templates/scene/index.js
@@ -1,13 +1,6 @@
 import React from "react"
-import tw from "tailwind.macro"
 import styled from "styled-components/macro"
 
-import Helmet from "components/Helmet"
-
 export default function __TEMPLATE_NAME__(props) {
-  return (
-    <>
-      <Helmet title="__TEMPLATE_NAME__" />
-    </>
-  )
+  return <></>
 }
